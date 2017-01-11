60603a5ef93b1d043848629f45a0abe88d8c9c67
djavorszky <daniel.javorszky@liferay.com>
2017-01-11 16:58:39 +0100

    Validate connection and database

--- mysql.go
+++ mysql.go
@@ -8,20 +8,44 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func validateConn() {
+var (
+	conn string
+	db   *sql.DB
+	err  error
+)
 
-	conn := fmt.Sprintf("%s:%s@/", conf.User, conf.Password)
-
-	db, err := sql.Open("mysql", conn)
+func prepDatabase() {
+	conn = fmt.Sprintf("%s:%s@/", conf.User, conf.Password)
+	db, err = sql.Open("mysql", conn)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	err = db.Ping()
+	validateConn()
+	validateMetaDB()
+}
+
+func validateConn() {
+
+	err := db.Ping()
 	if err != nil {
 		log.Fatalf("Could not validate database connection:\n\t%s", err.Error())
 	}
 
 	log.Println("MySQL connection validated")
+}
+
+func validateMetaDB() {
+	var count int
+
+	err := db.QueryRow("SELECT count(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?", "ddnc_info").Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if count == 1 {
+		return
+	}
+
 }
