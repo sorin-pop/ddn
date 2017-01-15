e64f005b72bb5d205fc0ea282c6e2052611f818b
djavorszky <daniel.javorszky@liferay.com>
2017-01-15 20:18:49 +0100

    Change process logic to call db.ImportDatabase

--- process.go
+++ process.go
@@ -1,34 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 )
 
 func startImport(dbreq DBRequest) {
-
 	log.Println("Starting download")
 
 	filepath, err := downloadFile(dbreq.DumpLocation)
 	if err != nil {
-		log.Println(err)
+		log.Println("Downloading file failed:", err.Error())
 	}
 	defer os.Remove(filepath)
 
 	log.Println("Download finished, starting import")
 
-	userArg, pwArg, dbnameArg, pathArg := fmt.Sprintf("-u %s", conf.User), fmt.Sprintf("-p%s", conf.Password), dbreq.DatabaseName, fmt.Sprintf("< %s", filepath)
-
-	log.Println(conf.Exec, userArg, pwArg, dbnameArg, pathArg)
-
-	cmd := exec.Command(conf.Exec, userArg, pwArg, dbnameArg, pathArg)
-
-	err = cmd.Run()
-	if err != nil {
-		log.Println("This fails all the time due to some reason.. The exact same command works from terminal.")
-		log.Fatal(err)
+	if err = db.ImportDatabase(dbreq); err != nil {
+		log.Println("Importing dump failed:", err.Error())
 	}
 
 	log.Println("Import finished.")
