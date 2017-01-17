5f2a1d31a140d6b7da7f27684a441b4832e4bc01
djavorszky <daniel.javorszky@liferay.com>
2017-01-17 09:42:54 +0100

    Change properties file creation

--- io.go
+++ io.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"runtime"
+	"text/template"
 )
 
 func generateProps(filename string) (string, error) {
@@ -12,18 +14,32 @@ func generateProps(filename string) (string, error) {
 	}
 	defer file.Close()
 
-	prop := `vendor="mysql"
-version="5.5.53"
-executable="/usr/bin/mysql"
-dbport="3306"
-connectorPort="7000"
-username="root"
-password="root"
-masterAddress="127.0.0.1"`
+	conf := Config{"mysql", "5.5.53", "/usr/bin/mysql", "3306", "7000", "root", "root", "127.0.0.1"}
 
-	_, err = file.WriteString(prop)
+	switch runtime.GOOS {
+	case "windows":
+		conf.Exec = "C:\\path\\to\\mysql.exe"
+	default:
+		conf.Exec = "/usr/bin/mysql"
+	}
+
+	prop := `vendor="{{.Vendor}}"
+version="{{.Version}}"
+executable="{{.Exec}}"
+dbport="{{.DBPort}}"
+connectorPort="{{.ConnectorPort}}"
+username="{{.User}}"
+password="{{.Password}}"
+masterAddress="{{.MasterAddress}}"`
+
+	tmpl, err := template.New("props").Parse(prop)
 	if err != nil {
-		return "", err
+		log.Fatal(err)
+	}
+
+	err = tmpl.Execute(file, conf)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	file.Sync()
