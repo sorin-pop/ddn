efc285c0eb3faf2ea1ccefd5c80c685264ad08b9
djavorszky <daniel.javorszky@liferay.com>
2017-02-01 17:17:34 +0100

    Implement extracting zip files

--- io.go
+++ io.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 	"text/template"
 )
 
@@ -47,4 +50,54 @@ masterAddress="{{.MasterAddress}}"
 	file.Sync()
 
 	return nil
+}
+
+func extractZip(filepath string) ([]string, error) {
+	r, err := zip.OpenReader(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("creating zip reader failed: %s", err.Error())
+	}
+	defer r.Close()
+
+	var files []string
+	for _, f := range r.File {
+		name, err := extractFile(f)
+		if err != nil {
+			return nil, fmt.Errorf("extracting zip file failed: %s", err.Error())
+		}
+
+		files = append(files, name)
+	}
+
+	return files, nil
+}
+
+func extractFile(f *zip.File) (string, error) {
+	src, err := f.Open()
+	if err != nil {
+		return "", fmt.Errorf("opening zipfile failed: %s", err.Error())
+	}
+	defer src.Close()
+
+	dst, err := os.OpenFile(f.Name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	if err != nil {
+		return "", fmt.Errorf("opening destination file failed: %s", err.Error())
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return "", fmt.Errorf("copying from archive failed: %s", err.Error())
+	}
+
+	return f.Name, nil
+}
+
+func isArchive(path string) bool {
+	switch filepath.Ext(path) {
+	case "zip", "tar", "tar.gz":
+		return true
+	}
+
+	return false
 }
--- process.go
+++ process.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"net/http"
 
@@ -11,19 +12,53 @@ import (
 
 func startImport(dbreq DBRequest) {
 	ch := notif.New(dbreq.ID, conf.MasterAddress)
+	defer close(ch)
 
 	ch <- notif.Y{StatusCode: http.StatusOK, Msg: "Starting download"}
 
-	filepath, err := downloadFile(dbreq.DumpLocation)
+	path, err := downloadFile(dbreq.DumpLocation)
 	if err != nil {
 		log.Printf("could not download file: %s", err.Error())
 
 		ch <- notif.Y{StatusCode: http.StatusInternalServerError, Msg: "Downlading file failed: " + err.Error()}
 		return
 	}
-	defer os.Remove(filepath)
+	defer os.Remove(path)
 
-	dbreq.DumpLocation = filepath
+	if isArchive(path) {
+		ch <- notif.Y{StatusCode: http.StatusOK, Msg: "Extracting archive"}
+
+		var files []string
+		switch filepath.Ext(path) {
+		case "zip":
+			files, err := extractZip(path)
+
+			if err != nil {
+				log.Printf("could not extract zip: %s", err.Error())
+
+				ch <- notif.Y{StatusCode: http.StatusInternalServerError, Msg: "Extracting file failed: " + err.Error()}
+				return
+			}
+			for _, f := range files {
+				defer os.Remove(f)
+			}
+
+			if len(files) > 1 {
+				log.Println("import process stopped; more than one file found in archive")
+
+				ch <- notif.Y{StatusCode: http.StatusBadRequest, Msg: "Archive contains more than one file, import stopped."}
+				return
+			}
+		default:
+			log.Println("import process stopped; encountered unsupported archive")
+
+			ch <- notif.Y{StatusCode: http.StatusBadRequest, Msg: "Unsupported archive."}
+			return
+		}
+		path = files[0]
+	}
+
+	dbreq.DumpLocation = path
 
 	ch <- notif.Y{StatusCode: http.StatusOK, Msg: "Starting import"}
 
@@ -36,5 +71,4 @@ func startImport(dbreq DBRequest) {
 		return
 	}
 	ch <- notif.Y{StatusCode: http.StatusOK, Msg: "Import finished successfully."}
-	close(ch)
 }
