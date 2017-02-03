7f21196ded555604b1e05ebfeaea719b7fa91fc6
sorin-pop <sorin.pop@liferay.com>
2017-02-03 10:46:34 +0100

    issue#26 refined the sql scripts

--- sql/oracle/create_schema.sql
+++ sql/oracle/create_schema.sql
@@ -1,4 +1,5 @@
-WHENEVER SQLERROR EXIT
+WHENEVER OSERROR EXIT FAILURE
+WHENEVER SQLERROR EXIT SQL.SQLCODE
 SET VERIFY OFF
 
 create user &1 identified by &2 default tablespace &3 quota unlimited on &3;
