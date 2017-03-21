773e8288a9cd00e6468fb1d3b2339410d256a8e1
djavorszky <daniel.javorszky@liferay.com>
2017-03-21 13:23:59 +0100

    [server] Add SIGKILL to the list of signals to catch

--- server/main.go
+++ server/main.go
@@ -27,10 +27,11 @@ func main() {
 	}()
 
 	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGKILL)
 	go func() {
 		<-c
-		// Received SIGTERM, do something.
+		// Received kill
+		log.Println("Received signal to terminate.")
 		os.Exit(1)
 	}()
 
