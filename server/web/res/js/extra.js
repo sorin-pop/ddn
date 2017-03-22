11df7e9c01a080f1a0bc056e70eb6cd21943e3be
djavorszky <daniel.javorszky@liferay.com>
2017-03-22 17:11:59 +0100

    [server] Disable dbname input if connector contains 'oracle', implements #50

--- server/web/res/js/extra.js
+++ server/web/res/js/extra.js
@@ -1,1 +1,20 @@
-alert("yes")
+$(document).ready(function() {
+    checkConnector()
+})
+
+$(document).on('change', '#connector', function() {
+    checkConnector()
+});
+    
+
+function checkConnector() {
+    var connector = $("#connector")
+    
+    if (connector.length != 0) {
+        if (connector.val().includes("oracle")) {
+            $("#dbname").prop('disabled', true);
+        } else {
+            $("#dbname").prop('disabled', false);
+        }
+    }
+}
