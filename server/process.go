b2d91a17d1bd569170e476eff74f64e9dba298ec
djavorszky <daniel.javorszky@liferay.com>
2017-03-27 16:42:01 +0200

    DDN-58 Send emails when expiring, dropping dbs

--- server/process.go
+++ server/process.go
@@ -19,7 +19,7 @@ import (
 //Second
 // Maintain should always be ran in a goroutine.
 func maintain() {
-	ticker := time.NewTicker(24 * time.Hour)
+	ticker := time.NewTicker(1000 * time.Millisecond)
 
 	for range ticker.C {
 		dbs, err := db.list()
@@ -40,16 +40,50 @@ func maintain() {
 
 				conn.DropDatabase(getID(), dbe.DBName, dbe.DBUser)
 				db.delete(int64(dbe.ID))
+
+				sendMail(dbe.Creator, fmt.Sprintf("[Cloud DB] Database %q dropped", dbe.DBName), fmt.Sprintf(`
+<h3>Database removal scheduled</h3>
+				
+<p>This is to inform you that the database %q has been dropped.</p>
+<p>Thank you for using <a href="http://cloud-db.liferay.int">Cloud DB</a>.</p>`, dbe.DBName))
+
+				continue
+			}
+
+			// if expires within a day:
+			// Note, not adding a check to see if an email has been sent
+			// already, as these are only checked once per day, meaning,
+			// on the next check the expiry date will be in the past.
+			dayPlus := now.AddDate(0, 0, 1)
+			if dbe.ExpiryDate.Before(dayPlus) {
+				sendMail(dbe.Creator, fmt.Sprintf("[Cloud DB] Database %q to be removed in 1 day", dbe.DBName), fmt.Sprintf(`
+<h3>Database removal imminent</h3>
+				
+<p>This is to inform you that the database %q will be removed in one day.</p>
+<p>If you'd like to extend it, please visit <a href="http://cloud-db.liferay.int">Cloud DB</a>.</p>
+<p>Cheers</p>`, dbe.DBName))
+
+				continue
+			}
+
+			if dbe.Status == status.RemovalScheduled {
+				continue
 			}
 
 			// if expires within a week:
 			weekPlus := now.AddDate(0, 0, 7)
 			if dbe.ExpiryDate.Before(weekPlus) {
 				db.updateColumn(dbe.ID, "status", status.RemovalScheduled)
+
+				sendMail(dbe.Creator, fmt.Sprintf("[Cloud DB] Database %q to be removed in one week", dbe.DBName), fmt.Sprintf(`
+<h3>Database removal scheduled</h3>
+				
+<p>This is to inform you that the database %q will be removed in 7 days.</p>
+<p>If you'd like to extend it, please visit <a href="http://cloud-db.liferay.int">Cloud DB</a>.</p>
+<p>Cheers</p>`, dbe.DBName))
 			}
 		}
 	}
-
 }
 
 // sendMail sends an email to "to" with subject "subj" and body "body".
@@ -81,8 +115,6 @@ func sendMail(to, subj, body string) error {
 	if err := dialer.DialAndSend(m); err != nil {
 		return fmt.Errorf("failed to send email: %s", err.Error())
 	}
-
-	log.Println("Email sent.")
 
 	return nil
 }
