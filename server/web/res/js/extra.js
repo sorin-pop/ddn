de8b7847c66d3e4ad06c35d185d0e34424fafc6b
djavorszky <daniel.javorszky@liferay.com>
2017-03-29 16:56:09 +0200

    DDN-68 Add client side user,dbname validation

--- server/web/res/js/extra.js
+++ server/web/res/js/extra.js
@@ -1,11 +1,44 @@
 $(document).ready(function() {
     checkConnector()
+    
+    $("#dbname").keyup(function() {
+        checkInputs()
+    });
+
+    $("#user").keyup(function() {
+        checkInputs()
+    })
+
 })
 
 $(document).on('change', '#connector', function() {
     checkConnector()
 });
-    
+
+function checkInputs() {
+    if (valid("#dbname") && valid("#user")) {
+        $("button").prop("disabled", false)
+    } else {
+        $("button").prop("disabled", true)
+    }
+}
+
+function valid(selector) {
+    var value = $(selector).val()
+    var pattern = "^[a-zA-Z0-9$_]+$"
+
+    if (value.match(pattern) || value == "") {
+        $(selector).parent().removeClass("has-danger")
+        $(selector).removeClass("form-control-danger")
+
+        return true
+    }
+
+    $(selector).parent().addClass("has-danger")
+    $(selector).addClass("form-control-danger")
+
+    return false
+}
 
 function checkConnector() {
     var connector = $("#connector")
