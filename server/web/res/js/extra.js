e1af7e11094d87a4a95a1399cc65b3f2cf5f9f1f
Dávid Balatoni <balcsida@gmail.com>
2017-06-04 14:02:54 +0200

    Beautify JS

--- server/web/res/js/extra.js
+++ server/web/res/js/extra.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     // Enable tooltips
-    $(function () {
+    $(function() {
         $('[data-toggle="tooltip"]').tooltip()
         $('[data-toggle="popover"]').popover()
     })
 
 
     checkConnector()
-    
+
     $("#dbname").keyup(function() {
         checkInputs()
     });
@@ -68,7 +68,7 @@ function checkConnector() {
 
             $("#user").prop('disabled', true);
             $("#password").prop('disabled', true);
-            
+
             $("#userdiv").attr('title', msg).attr('data-original-title', msg).tooltip('hide');
         }
     }
