264f22593c089ca513e55a7da3a4b901ef8d65a1
Daniel Javorszky <daniel.javorszky@liferay.com>
2017-08-21 14:31:19 +0200

    Add dockerfile and deploy script

---
+++ dist/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "building binary.."
+cd ../server
+go build
+
+echo "archiving.."
+tar czf ddn-server.tar.gz server web
+cd ../out
+
+mv -v ../server/ddn-server.tar.gz .
+
+echo "building image"
+docker build -t ddn/server .
+
+echo "starting container.."
+docker run -dit -p 7010:7010 -v `pwd`/data:/ddn/data ddn/server:latest
+
+echo "removing artefacts.."
+rm -rf data ddn-server.tar.gz
